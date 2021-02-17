88f177d8059170041afad6f83b243924aa421f0e
Stefan <silva.stefan@gmail.com>
2021-02-17 00:41:09 -0300

    teste final merge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth - update xxx...olaaaaaaa!</h1>
+        <h1>We now have Auth!</h1>
       </header>
       <AmplifySignOut />
     </div>
